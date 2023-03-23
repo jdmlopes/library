36e1fc01b1529ce871cc3db28a72884b911c54f6
jdmlopes <jdlopes97@gmail.com>
2023-03-23 12:03:57 -0300

    Create function that inserts cards to DOM

--- script.js
+++ script.js
@@ -8,76 +8,91 @@ function Book(title, author, pages, read) {
   this.read = read;
 }
 
-function addBook() {
-  let title = "A Test Book";
-  let author = "John Tester";
-  let pages = 200;
-  let read = false;
+function addBookToLibrary(title, author, pages, read) {
   const book = new Book(title, author, pages, read);
   insertBook(book);
+}
 
-  const bookElement = document.createElement("div");
-  bookElement.classList.add("card");
-  bookElement.setAttribute("data-index", `${library.length - 1}`);
+function insertCardsToDOM() {
+  for (const book of library) {
+    const bookElement = createDOMElement("div", "card", "");
+    bookElement.setAttribute("data-index", `${library.length - 1}`);
 
-  const titleElement = document.createElement("div");
-  titleElement.classList.add("title");
-  titleElement.textContent = book.title;
+    const titleElement = createDOMElement("div", "title", book.title);
 
-  const authorElement = document.createElement("div");
-  authorElement.classList.add("author");
-  authorElement.textContent = "Author: " + book.author;
+    const authorElement = createDOMElement(
+      "div",
+      "author",
+      `Author: ${book.author}`
+    );
 
-  const pagesNumberElement = document.createElement("div");
-  pagesNumberElement.classList.add("pages");
-  pagesNumberElement.textContent = "Pages: " + book.pages;
+    const pagesNumberElement = createDOMElement(
+      "div",
+      "page-number",
+      `Pages: ${book.pages}`
+    );
 
-  const bookInfoElement = document.createElement("div");
-  bookInfoElement.classList.add("book-info");
-  bookInfoElement.appendChild(authorElement);
-  bookInfoElement.appendChild(pagesNumberElement);
+    const bookInfoElement = createDOMElement("div", "book-info");
 
-  const readFlagElement = document.createElement("div");
-  readFlagElement.classList.add("read-flag");
+    bookInfoElement.appendChild(authorElement);
+    bookInfoElement.appendChild(pagesNumberElement);
 
-  const readLabelElement = document.createElement("label");
-  const readCheckboxElement = document.createElement("input");
+    const readFlagElement = createDOMElement("div", "read-flag");
 
-  readLabelElement.textContent = "Finished Reading? ";
-  readLabelElement.setAttribute("for", "read");
-  readCheckboxElement.setAttribute("type", "checkbox");
+    const readButton = createDOMElement("button", "read-btn", "Not Read");
+    if (book.read) {
+      readButton.classList.add("read");
+      readButton.textContent = "Read";
+    }
+    readFlagElement.appendChild(readButton);
 
-  readFlagElement.appendChild(readLabelElement);
-  readFlagElement.appendChild(readCheckboxElement);
+    const cardOptionsElement = createDOMElement("div", "card-options");
 
-  const cardOptionsElement = document.createElement("div");
-  cardOptionsElement.classList.add("card-options");
-  cardOptionsElement.innerHTML = `<div class="edit">
-  <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
-    <title>Edit</title>
-    <path
-      d="M5,3C3.89,3 3,3.89 3,5V19A2,2 0 0,0 5,21H19A2,2 0 0,0 21,19V12H19V19H5V5H12V3H5M17.78,4C17.61,4 17.43,4.07 17.3,4.2L16.08,5.41L18.58,7.91L19.8,6.7C20.06,6.44 20.06,6 19.8,5.75L18.25,4.2C18.12,4.07 17.95,4 17.78,4M15.37,6.12L8,13.5V16H10.5L17.87,8.62L15.37,6.12Z"
-    />
-  </svg>
-</div>
-<div class="delete">
-  <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
-    <title>Delete</title>
-    <path
-      d="M9,3V4H4V6H5V19A2,2 0 0,0 7,21H17A2,2 0 0,0 19,19V6H20V4H15V3H9M7,6H17V19H7V6M9,8V17H11V8H9M13,8V17H15V8H13Z"
-    />
-  </svg>`;
+    cardOptionsElement.innerHTML = `<div class="edit">
+        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
+          <title>Edit</title>
+          <path
+            d="M5,3C3.89,3 3,3.89 3,5V19A2,2 0 0,0 5,21H19A2,2 0 0,0 21,19V12H19V19H5V5H12V3H5M17.78,4C17.61,4 17.43,4.07 17.3,4.2L16.08,5.41L18.58,7.91L19.8,6.7C20.06,6.44 20.06,6 19.8,5.75L18.25,4.2C18.12,4.07 17.95,4 17.78,4M15.37,6.12L8,13.5V16H10.5L17.87,8.62L15.37,6.12Z"
+          />
+        </svg>
+      </div>
+      <div class="delete">
+        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
+          <title>Delete</title>
+          <path
+            d="M9,3V4H4V6H5V19A2,2 0 0,0 7,21H17A2,2 0 0,0 19,19V6H20V4H15V3H9M7,6H17V19H7V6M9,8V17H11V8H9M13,8V17H15V8H13Z"
+          />
+        </svg>`;
 
-  bookElement.appendChild(titleElement);
-  bookElement.appendChild(bookInfoElement);
-  bookElement.appendChild(readFlagElement);
-  bookElement.appendChild(cardOptionsElement);
+    bookElement.appendChild(titleElement);
+    bookElement.appendChild(bookInfoElement);
+    bookElement.appendChild(readFlagElement);
+    bookElement.appendChild(cardOptionsElement);
 
-  books.appendChild(bookElement);
+    books.appendChild(bookElement);
+  }
+}
+
+function createDOMElement(elementName, className, textContent = "") {
+  const element = document.createElement(elementName);
+  if (className !== "") element.classList.add(className);
+  element.textContent = textContent;
+  return element;
 }
 
 function insertBook(book) {
   library.push(book);
 }
 
-addBook();
+addBookToLibrary(
+  "The Chronicles of Narnia: The Lion, The Witch and The Wardrobe",
+  "Num Sei",
+  600,
+  true
+);
+addBookToLibrary("Chuck Testa", "John Tester", 200, false);
+addBookToLibrary("Chuck Testa 2", "John Tester", 250, true);
+addBookToLibrary("Chuck Testa 3", "John Tester", 100, false);
+addBookToLibrary("Chuck Testa 4: Tokyo Drift ", "John Tester", 300, true);
+addBookToLibrary("Chuck Testa: The Return", "John Tester", 800, false);
+insertCardsToDOM();
