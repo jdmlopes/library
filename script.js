674996a0928fa1c04dab9ff74b545ffd1f5b0ea6
jdmlopes <jdlopes97@gmail.com>
2023-03-23 21:02:03 -0300

    Implement delete button functionality

--- script.js
+++ script.js
@@ -17,7 +17,9 @@ function addBookToLibrary(title, author, pages, read) {
   insertBook(book);
 }
 
+//Erases the book card list and inserts all of them
 function insertCardsToDOM() {
+  books.innerHTML = "";
   for (const [index, book] of library.entries()) {
     const bookElement = createDOMElement("div", "card", "");
     bookElement.setAttribute("data-index", `${index}`);
@@ -45,29 +47,37 @@ function insertCardsToDOM() {
 
     const readButton = createDOMElement("button", "read-btn", "Not Read");
     readButton.setAttribute("data-index", `${index}`);
+    readButton.addEventListener(
+      "click",
+      toggleRead.bind(null, readButton, index)
+    );
     if (book.read) {
       readButton.classList.add("read");
       readButton.textContent = "Read";
     }
     readFlagElement.appendChild(readButton);
 
-    const cardOptionsElement = createDOMElement("div", "card-options");
+    const editButton = createDOMElement("div", "edit", "");
+    editButton.innerHTML = `
+      <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
+        <title>Edit</title>
+        <path d="M5,3C3.89,3 3,3.89 3,5V19A2,2 0 0,0 5,21H19A2,2 0 0,0 21,19V12H19V19H5V5H12V3H5M17.78,4C17.61,4 17.43,4.07 17.3,4.2L16.08,5.41L18.58,7.91L19.8,6.7C20.06,6.44 20.06,6 19.8,5.75L18.25,4.2C18.12,4.07 17.95,4 17.78,4M15.37,6.12L8,13.5V16H10.5L17.87,8.62L15.37,6.12Z"/>
+      </svg>
+    `;
 
-    cardOptionsElement.innerHTML = `<div class="edit">
-        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
-          <title>Edit</title>
-          <path
-            d="M5,3C3.89,3 3,3.89 3,5V19A2,2 0 0,0 5,21H19A2,2 0 0,0 21,19V12H19V19H5V5H12V3H5M17.78,4C17.61,4 17.43,4.07 17.3,4.2L16.08,5.41L18.58,7.91L19.8,6.7C20.06,6.44 20.06,6 19.8,5.75L18.25,4.2C18.12,4.07 17.95,4 17.78,4M15.37,6.12L8,13.5V16H10.5L17.87,8.62L15.37,6.12Z"
-          />
-        </svg>
-      </div>
-      <div class="delete">
-        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
-          <title>Delete</title>
-          <path
-            d="M9,3V4H4V6H5V19A2,2 0 0,0 7,21H17A2,2 0 0,0 19,19V6H20V4H15V3H9M7,6H17V19H7V6M9,8V17H11V8H9M13,8V17H15V8H13Z"
-          />
-        </svg>`;
+    const deleteButton = createDOMElement("div", "delete", "");
+    deleteButton.innerHTML = `
+      <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
+        <title>Delete</title>
+        <path
+        d="M9,3V4H4V6H5V19A2,2 0 0,0 7,21H17A2,2 0 0,0 19,19V6H20V4H15V3H9M7,6H17V19H7V6M9,8V17H11V8H9M13,8V17H15V8H13Z"
+        />
+      </svg>
+    `;
+    deleteButton.addEventListener("click", deleteBook.bind(null, index));
+    const cardOptionsElement = createDOMElement("div", "card-options");
+    cardOptionsElement.appendChild(editButton);
+    cardOptionsElement.appendChild(deleteButton);
 
     bookElement.appendChild(titleElement);
     bookElement.appendChild(bookInfoElement);
@@ -89,13 +99,18 @@ function insertBook(book) {
   library.push(book);
 }
 
-function toggleRead(element) {
-  library[+element.dataset.index].toggleRead();
-  if (element.classList.toggle("read")) {
-    element.textContent = "Read";
+function toggleRead(button, index) {
+  library[index].toggleRead();
+  if (button.classList.toggle("read")) {
+    button.textContent = "Read";
     return;
   }
-  element.textContent = "Not Read";
+  button.textContent = "Not Read";
+}
+
+function deleteBook(index) {
+  library.splice(index, 1);
+  insertCardsToDOM();
 }
 
 addBookToLibrary(
@@ -111,8 +126,3 @@ addBookToLibrary("Chuck Testa 4: Tokyo Drift ", "John Tester", 300, true);
 addBookToLibrary("Chuck Testa: The Return", "John Tester", 800, false);
 
 insertCardsToDOM();
-
-const readButtons = document.querySelectorAll(".read-btn");
-readButtons.forEach((btn) => {
-  btn.addEventListener("click", toggleRead.bind(null, btn));
-});
