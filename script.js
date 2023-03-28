655f5850c586f948432c393b0499aa88e314bc8d
jdmlopes <jdlopes97@gmail.com>
2023-03-28 19:13:16 -0300

    Refactor Book to use class instead of function

--- script.js
+++ script.js
@@ -3,17 +3,6 @@ const modal = document.querySelector("#modal");
 const bookForm = document.querySelector("#insert-book-form");
 const library = [];
 
-/* function Book(title, author, pages, read) {
-  this.title = title;
-  this.author = author;
-  this.pages = pages;
-  this.read = read;
-}
-
-Book.prototype.toggleRead = function () {
-  this.read = this.read ? false : true;
-}; */
-
 class Book {
   constructor(title, author, pages, read) {
     this.title = title;
